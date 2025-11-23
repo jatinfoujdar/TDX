3da938299478b8975526ba4aa328e03b4b001d7b
Jatin Foujdar <jatin321001@gmail.com>
2025-11-23 13:33:24 +0530

    Initial Commit

---
+++ TDX/TDXApp.swift
@@ -0,0 +1,17 @@
+//
+//  TDXApp.swift
+//  TDX
+//
+//  Created by jatin foujdar on 23/11/25.
+//
+
+import SwiftUI
+
+@main
+struct TDXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
